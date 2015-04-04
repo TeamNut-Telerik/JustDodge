66de2142662c022ccc5c0b67aaf74512347b14e1
AntonioFilipov <tonkatafilipov@gmail.com>
2015-04-05 00:36:45 +0300

    add abstract class Bonus and IBonus

---
+++ Scripts/Bonus.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public abstract class Bonus:IBonus
+    {
+        private string name;
+        private DateTime duration;
+
+        public DateTime Duration
+        {
+            get { return this.duration; }
+            set { this.duration = value; }
+        }
+        
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        
+    }
+}
