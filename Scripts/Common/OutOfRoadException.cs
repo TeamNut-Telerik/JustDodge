64cadf8231765c0b4d5b0d789692dd3aaeb3dde0
AntonioFilipov <tonkatafilipov@gmail.com>
2015-04-07 13:10:59 +0300

    add exceptions
    not implemented yet

---
+++ Scripts/Common/OutOfRoadException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Common
+{
+    class OutOfRoadException:Exception
+    {
+        public OutOfRoadException(string message)
+        : base(message)
+        {
+        }
+    }
+}
