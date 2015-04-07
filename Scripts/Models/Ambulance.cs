6f98655cb0f58afdd5a76025c7cfde7e058190e7
MarinMarinov <marin.telerik@gmail.com>
2015-04-07 15:57:41 +0300

    Added Ambulance class, inheritor of EnemyCar abstract class.

---
+++ Scripts/Models/Ambulance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Assets.Scripts.Enumerations;
+
+namespace Assets.Scripts.Models
+{
+    public class Ambulance : EnemyCar
+    {
+        public Ambulance(EManufacturer manufacturer, float maxSpeed, float price, EColor color) :
+            base(manufacturer, maxSpeed, price, color)
+        {
+        }
+    }
+}
